6784411958d27c3d6db8bb12641b45f3e1e5be67
Marco <marcoancorrea@gmail.com>
2020-02-13 13:52:11 +0100

    Adding icons and random animations

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
 var time = 0
+var iconNames = [
+  "icon_bus.png",
+  "icon_s.png",
+  "icon_tourist.png",
+  "icon_train.png",
+  "icon_tram.png"
+]
+
 function startTimer() {
   setInterval(setTime, 1000)
 }
 
 function setTime() {
   ++time
-  var secondsLabel = document.getElementById("timer")
-  secondsLabel.innerHTML = time
+  var timerLabel = document.getElementById("timer")
+  timerLabel.innerHTML = time
 }
 
 function move() {
-  var icon = document.getElementById("icon")
+  changeAnimationTime()
+  icon.addEventListener("animationiteration", AnimationListener, false)
   icon.classList.add("move-right")
 }
 
-function setProperty(duration) {
-  icon.style.setProperty("--animation-time", duration + "s")
+function AnimationListener() {
+  icon.style.webkitAnimation = "none"
+  var currentTime = getComputedStyle(icon).getPropertyValue("--animation-time")
+  currentTime = currentTime.slice(0, -1).trim()
+  changeAnimationTime()
+  setRandomIcon()
+  icon.style.webkitAnimation = ""
+}
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function setRandomIcon() {
+  var randomIcon = iconNames[randomInteger(0, this.iconNames.length - 1)]
+  icon.src = "/icons/" + randomIcon
 }
 
 function changeAnimationTime() {
-  var min = 2
-  var max = 6
-  var randomInteger = min + Math.floor((max - min) * Math.random())
-  setProperty(randomInteger)
+  icon.style.setProperty("--animation-time", randomInteger(2, 5) + "s")
 }
 
-/* setInterval(changeAnimationTime, 1000); */
 window.onload = function() {
+  var icon = document.getElementById("icon")
+  setRandomIcon()
   startTimer()
   move()
 }
